2739bc016b6c0d160ff398b23749d0581dfb27dd
Joseph Gulay <joegulay@gmail.com>
2022-03-20 09:03:49 +1100

    Add a predict function to the fast.py file

--- api/fast.py
+++ api/fast.py
@@ -21,54 +21,60 @@ def index():
     return {"greeting": "Hello world"}
 
 
-# @app.get("/predict")
-# def api_predict(pickup_datetime, pickup_longitude, pickup_latitude,
-#                 dropoff_longitude, dropoff_latitude, passenger_count):
+@app.get("/predict")
+def api_predict(storage):
+    return {"storage location": storage}
 
-#     # Put the inputs into a dictionary
-#     response_dict = {
-#         "key": f"{pickup_datetime}.0019",  #The hardcoded value
-#         "pickup_datetime": pickup_datetime,
-#         "pickup_longitude": pickup_longitude,
-#         "pickup_latitude": pickup_latitude,
-#         "dropoff_longitude": dropoff_longitude,
-#         "dropoff_latitude": dropoff_latitude,
-#         "passenger_count": passenger_count
-#     }
 
-#     # Create a dataframe from a dictionary
-#     df = pd.DataFrame(response_dict, index=[0])
 
-#     #Convert the values into the correct data types
-#     df.pickup_longitude = df.pickup_longitude.astype('float64')
-#     df.pickup_latitude = df.pickup_latitude.astype('float64')
-#     df.dropoff_longitude = df.dropoff_longitude.astype('float64')
-#     df.dropoff_latitude = df.dropoff_latitude.astype('float64')
-#     df.passenger_count = df.passenger_count.astype('int64')
 
-#     # create a datetime object from the user provided datetime
-#     pickup_datetime = df.pickup_datetime[0]
-#     pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+def api_predict(pickup_datetime, pickup_longitude, pickup_latitude,
+                dropoff_longitude, dropoff_latitude, passenger_count):
 
-#     # localize the user datetime with NYC timezone
-#     eastern = pytz.timezone("US/Eastern")
-#     localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    # Put the inputs into a dictionary
+    response_dict = {
+        "key": f"{pickup_datetime}.0019",  #The hardcoded value
+        "pickup_datetime": pickup_datetime,
+        "pickup_longitude": pickup_longitude,
+        "pickup_latitude": pickup_latitude,
+        "dropoff_longitude": dropoff_longitude,
+        "dropoff_latitude": dropoff_latitude,
+        "passenger_count": passenger_count
+    }
 
-#     # localize the datetime to UTC
-#     utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    # Create a dataframe from a dictionary
+    df = pd.DataFrame(response_dict, index=[0])
 
-#     # convert the datetime to the format expected by the pipeline
-#     formatted_pickup_datetime = utc_pickup_datetime.strftime(
-#         "%Y-%m-%d %H:%M:%S UTC")
+    #Convert the values into the correct data types
+    df.pickup_longitude = df.pickup_longitude.astype('float64')
+    df.pickup_latitude = df.pickup_latitude.astype('float64')
+    df.dropoff_longitude = df.dropoff_longitude.astype('float64')
+    df.dropoff_latitude = df.dropoff_latitude.astype('float64')
+    df.passenger_count = df.passenger_count.astype('int64')
 
-#     # put the data back into the dataframe
-#     df.pickup_datetime = formatted_pickup_datetime
+    # create a datetime object from the user provided datetime
+    pickup_datetime = df.pickup_datetime[0]
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
 
-#     # Call the predict function
-#     X = df  #.drop(columns = 'key')
-#     print("1")
-#     model = predict.download_model()
-#     print("2")
-#     prediction = predict.make_prediction(model, X)
+    # localize the user datetime with NYC timezone
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
 
-#     return {"prediction": prediction[0]}
+    # localize the datetime to UTC
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+
+    # convert the datetime to the format expected by the pipeline
+    formatted_pickup_datetime = utc_pickup_datetime.strftime(
+        "%Y-%m-%d %H:%M:%S UTC")
+
+    # put the data back into the dataframe
+    df.pickup_datetime = formatted_pickup_datetime
+
+    # Call the predict function
+    X = df  #.drop(columns = 'key')
+    print("1")
+    model = predict.download_model()
+    print("2")
+    prediction = predict.make_prediction(model, X)
+
+    return {"prediction": prediction[0]}
