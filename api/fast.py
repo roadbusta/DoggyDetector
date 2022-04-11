4cb0a35bfef44b824a86b5591c154122e4d39358
Joseph Gulay <joegulay@gmail.com>
2022-04-12 08:13:00 +1000

    Change deployment memory to 1 GB and uncomment commented code on fast.py

--- api/fast.py
+++ api/fast.py
@@ -53,7 +53,7 @@ def predict_breed(BUCKET_NAME, BLOB_NAME):
             storage_client.download_blob_to_file(blob,f)
 
 
-        #Load the model if from pickle
+        # # Load the model if from pickle
         # BUCKET_PICKLE_LOCATION = "models/Inception/V1/model.joblib"
         # model_pickle = pickle_from_gcp(BUCKET_NAME= BUCKET_NAME,
         #                                BUCKET_PICKLE_LOCATION= BUCKET_PICKLE_LOCATION)
@@ -61,27 +61,28 @@ def predict_breed(BUCKET_NAME, BLOB_NAME):
         # model = pickle.loads(model_pickle)
 
 
-        #Load the model if from model.joblib
+        # Load the model if from model.joblib
 
-        #BUCKET_MODEL_LOCATION = "models/Inception/V1/model.joblib"
-        #MODEL_FILE_PATH = os.path.join(os.getcwd(), 'model.joblib') #This is where it is saved locally
-        #file_from_gcp(BUCKET_NAME=BUCKET_NAME,
-        #BUCKET_PICKLE_LOCATION=BUCKET_MODEL_LOCATION,
-        #DESTINATION_FILE_NAME=MODEL_FILE_PATH)
-
-
-    # model = joblib.load(MODEL_FILE_PATH)
-
-    # #Run predict
+        BUCKET_MODEL_LOCATION = "models/Inception/V1/model.joblib"
+        MODEL_FILE_PATH = os.path.join(os.getcwd(), 'model.joblib') #This is where it is saved locally
+        file_from_gcp(BUCKET_NAME=BUCKET_NAME,
+        BUCKET_PICKLE_LOCATION=BUCKET_MODEL_LOCATION,
+        DESTINATION_FILE_NAME=MODEL_FILE_PATH)
 
 
-    # predictor = Predictor()
+        model = joblib.load(MODEL_FILE_PATH)
 
-    #prediction = predictor.predict(image_path = IMAGE_FILE_PATH, model = model)
+        #Run predict
 
-    #return prediction
 
-        return {"outcome": "successfully loaded object"}
+        predictor = Predictor()
+
+        prediction = predictor.predict(image_path = IMAGE_FILE_PATH, model = model)
+
+
+
+        return {"outcome": "successfully loaded object",
+                "prediction" :  prediction}
 
 
     except Exception as e:
